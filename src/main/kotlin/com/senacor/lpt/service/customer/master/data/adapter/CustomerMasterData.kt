7684cc15701dbfa4fb13e89104602d7117c9ab60
frudisch <florian.rudisch@gmail.com>
2024-10-28 14:24:53 +0100

    Add wiremock as customer master data system and connect with credit application system

---
+++ src/main/kotlin/com/senacor/lpt/service/customer/master/data/adapter/CustomerMasterData.kt
@@ -0,0 +1,7 @@
+package com.senacor.lpt.service.customer.master.data.adapter
+
+import java.time.LocalDate
+
+data class CustomerMasterData(val firstname: String,
+                              val lastname: String,
+                              val birthday: LocalDate)
