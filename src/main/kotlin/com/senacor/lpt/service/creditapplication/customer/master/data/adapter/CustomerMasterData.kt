7dba200e52575a2eaf5c8b771cc0c42e35a4fec7
frudisch <florian.rudisch@gmail.com>
2024-11-11 21:45:53 +0100

    Fix incorrect date format in customer master data

--- src/main/kotlin/com/senacor/lpt/service/creditapplication/customer/master/data/adapter/CustomerMasterData.kt
+++ src/main/kotlin/com/senacor/lpt/service/creditapplication/customer/master/data/adapter/CustomerMasterData.kt
@@ -1,9 +1,11 @@
 package com.senacor.lpt.service.creditapplication.customer.master.data.adapter
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDate
 
 data class CustomerMasterData(
     val firstname: String,
     val lastname: String,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy")
     val birthday: LocalDate
 )
