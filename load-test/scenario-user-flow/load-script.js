ad848ea961f412733849733a14c0a497c3c04157
frudisch <florian.rudisch@gmail.com>
2024-11-26 21:24:37 +0100

    This commit contains all the changes made by hand during the first load test session

--- load-test/scenario-user-flow/load-script.js
+++ load-test/scenario-user-flow/load-script.js
@@ -7,7 +7,7 @@ const creditApplicationAcceptanceRate = new Rate('failed_credit_application_acce
 
 export const options = {
     vus: 10,
-    duration: '30s',
+    duration: '60s',
     thresholds: {
         'failed_credit_application_creation': ['rate<0.1'],
         'failed_credit_application_acceptance': ['rate<0.1'],
@@ -16,19 +16,17 @@ export const options = {
 };
 
 export default function () {
-    const creditApplicationCreation = http.post('http://creditapplication-service.lpt.svc.cluster.local:8080/api/credit-applications', {
-        "creditAmount":10000.00,
-        "firstName":"Harry",
-        "lastName":"Potter",
-        "zipCode":"12345",
-        "occupation":"IT",
-        "monthlyNetIncome":3000.00,
-        "monthlyExpenses":2500.00
+    const creditApplicationCreation = http.post('http://creditapplication-k8s-service.lpt.svc.cluster.local:8080/api/credit-applications', JSON.stringify({"creditAmount":10000.00,"firstName":"Jim","lastName":"Beam","zipCode":"12345","occupation":"IT","monthlyNetIncome":5000.00,"monthlyExpenses":2500.00}), {
+        headers: { 'Content-Type': 'application/json' },
     });
-    creditApplicationCreationRate.add(creditApplicationCreation.status !== 201);
+    console.log(creditApplicationCreation.status)
+    console.log(creditApplicationCreation.body)
+    creditApplicationCreationRate.add(creditApplicationCreation.status !== 200);
     const applicationId = creditApplicationCreation.body.id;
     // after receiving the credit decision, the customer decides within 5 seconds to accept or decline the credit application
     sleep(5);
-    const creditApplicationAcceptance = http.post(`http://creditapplication-service.lpt.svc.cluster.local:8080/api/credit-applications/${applicationId}`, {});
-    creditApplicationAcceptanceRate.add(creditApplicationAcceptance.status !== 200);
+    const creditApplicationAcceptance = http.post(`http://creditapplication-k8s-service.lpt.svc.cluster.local:8080/api/credit-applications/${applicationId}`, JSON.stringify({}), {
+        headers: { 'Content-Type': 'application/json' },
+    });
+    creditApplicationAcceptanceRate.add(creditApplicationAcceptance.status !== 202);
 }
